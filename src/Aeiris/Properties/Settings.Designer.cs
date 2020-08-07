2492edae94f185aa9f9f8c62a6d9e29dbc2c3bb9
Yune Sao Sung <69162537+YuneSung@users.noreply.github.com>
2020-08-08 00:57:52 +0100

    Update

--- src/Aeiris/Properties/Settings.Designer.cs
+++ src/Aeiris/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Aeiris.Properties {
+namespace Aeiris.dev.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
